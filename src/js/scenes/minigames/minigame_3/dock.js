7115d5edc9b079f32a470bc58ebc412728fdb6c2
Zhi-CMGT <0980617@hr.nl>
2025-06-16 13:14:36 +0200

    image minigame3 toegevoegd

---
+++ src/js/scenes/minigames/minigame_3/dock.js
@@ -0,0 +1,10 @@
+import { Actor } from "excalibur";
+
+export class Dock extends Actor {
+    constructor(x, y, width, height) {
+
+        super({
+
+        });
+    }
+}
---
+++ src/js/scenes/minigames/minigame_3/sea.js
@@ -0,0 +1,10 @@
+import { Actor } from "excalibur";
+
+export class Sea extends Actor {
+    constructor(x, y, width, height) {
+
+        super({
+
+        });
+    }
+}
