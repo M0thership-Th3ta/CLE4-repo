b99bee70be431ba0c85108c8bf2880dc64254613
AirMile <zeilstramiles@gmail.com>
2025-06-16 16:00:09 +0200

    restuarant sprite

--- src/js/resources.js
+++ src/js/resources.js
@@ -14,8 +14,11 @@ const Resources = {
     Farmer: new ImageSource('images/pokemonFarmer.png'),
 }
 
-
-
+// Voeg error handling toe voor elke resource
+Object.entries(Resources).forEach(([name, resource]) => {
+    resource.onLoad = () => console.log(`✓ ${name} geladen`)
+    resource.onError = (error) => console.error(`✗ Fout bij laden ${name}:`, error)
+})
 
 const ResourceLoader = new Loader()
 for (let res of Object.values(Resources)) {
