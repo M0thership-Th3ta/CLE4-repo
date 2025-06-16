a9a4153be78db18705ab7dce277551a066d4961a
AirMile <zeilstramiles@gmail.com>
2025-06-16 13:44:44 +0200

    cutscenes to debug and game

--- src/js/game.js
+++ src/js/game.js
@@ -5,6 +5,11 @@ import { DebugControl } from './debug_control.js'
 import { Minigame_1 } from './scenes/minigames/minigame_1/minigame_1.js'
 import { Minigame_2 } from './scenes/minigames/minigame_2/minigame_2.js'
 import { Minigame_3 } from './scenes/minigames/minigame_3/minigame_3.js'
+import { Cutscene_1 } from './scenes/cutscenes/cutscene_1.js'
+import { Cutscene_2 } from './scenes/cutscenes/cutscene_2.js'
+import { Cutscene_3 } from './scenes/cutscenes/cutscene_3.js'
+import { Cutscene_4 } from './scenes/cutscenes/cutscene_4.js'
+import { Testscene } from './scenes/cutscenes/testscene.js'
 
 export class Game extends Engine {
 
@@ -20,9 +25,14 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
+        this.add('cutscene_1', new Cutscene_1())
         this.add('minigame_1', new Minigame_1())
+        this.add('cutscene_2', new Cutscene_2())
         this.add('minigame_2', new Minigame_2())
+        this.add('cutscene_3', new Cutscene_3())
         this.add('minigame_3', new Minigame_3())
+        this.add('cutscene_1', new Cutscene_4())
+        this.add('testscene', new Testscene())
         this.goToScene('root')
 
         this.debugControl = new DebugControl(this)
