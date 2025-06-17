ac3a1e6b01edb33f09e368821863e3da3637f508
anemoia <killboy1100@gmail.com>
2025-06-17 14:52:52 +0200

    Functional Food Items in Minigame 2

--- src/js/scenes/minigames/minigame_2/food.js
+++ src/js/scenes/minigames/minigame_2/food.js
@@ -1,13 +1,33 @@
 import { Actor, Engine, Vector, CollisionType, Shape, RotationType } from "excalibur"
 import { Resources } from '../../../resources.js'
 
-export class Food extends Actor {
+export class Food1 extends Actor {
     // Geef aan dat dit object interactable is
     isInteractible = true
 
-    constructor(pos, resource) {
+    constructor(pos) {
         super({
-            resource,
+            pos,
+            width: Resources.Food1.width,
+            height: Resources.Food1.height,
+            scale: new Vector(0.5, 0.5),
+            collisionType: CollisionType.Active
+        })
+    }
+
+    // Deze functie wordt één keer aangeroepen wanneer het food item wordt toegevoegd
+    onInitialize(engine) {
+        // Zet de sprite van het food item
+        this.graphics.use(Resources.Food1.toSprite())
+    }
+}
+
+export class Food2 extends Actor {
+    // Geef aan dat dit object interactable is
+    isInteractible = true
+
+    constructor(pos) {
+        super({
             pos,
             width: 32,
             height: 32,
@@ -19,6 +39,27 @@ export class Food extends Actor {
     // Deze functie wordt één keer aangeroepen wanneer het food item wordt toegevoegd
     onInitialize(engine) {
         // Zet de sprite van het food item
-        this.graphics.use(this.resource.toSprite())
+        this.graphics.use(Resources.Food2.toSprite())
+    }
+}
+
+export class Food3 extends Actor {
+    // Geef aan dat dit object interactable is
+    isInteractible = true
+
+    constructor(pos) {
+        super({
+            pos,
+            width: 32,
+            height: 32,
+            scale: new Vector(0.5, 0.5),
+            collisionType: CollisionType.Active
+        })
+    }
+
+    // Deze functie wordt één keer aangeroepen wanneer het food item wordt toegevoegd
+    onInitialize(engine) {
+        // Zet de sprite van het food item
+        this.graphics.use(Resources.Food3.toSprite())
     }
 }
--- src/js/scenes/minigames/minigame_2/minigame_2.js
+++ src/js/scenes/minigames/minigame_2/minigame_2.js
@@ -3,13 +3,9 @@ import { Resources, ResourceLoader } from '../../../resources.js';
 import { Player } from '../../../player/robot/player.js';
 import { Pointer } from '../../../player/robot/pointer.js';
 import { Background2 } from "./background_2.js";
-import { Food } from "./food.js";
+import { Food1, Food2, Food3 } from "./food.js";
 
 export class Minigame_2 extends Scene {
-    Food1 = Resources.Food1
-    Food2 = Resources.Food2
-    Food3 = Resources.Food3
-
     constructor() {
         super()
     }
@@ -25,8 +21,8 @@ export class Minigame_2 extends Scene {
         const pointer = new Pointer(new Vector(200,200))
         this.add(pointer)
         this.add(new Background2());
-        this.add(new Food(new Vector(100, 100), Food1));
-        this.add(new Food(new Vector(300, 100), Food2));
-        this.add(new Food(new Vector(200, 200), Food3));
+        this.add(new Food1(new Vector(100, 100)));
+        this.add(new Food2(new Vector(300, 100)));
+        this.add(new Food3(new Vector(200, 200)));
     }
 }
