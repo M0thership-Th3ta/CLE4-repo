fe1e9e2d2d53552b953d0d04609a2fb7abd35951
Zhi-CMGT <0980617@hr.nl>
2025-06-26 12:54:40 +0200

    font eindscene aangepast

--- src/js/scenes/cutscenes/restaurantscene_4.js
+++ src/js/scenes/cutscenes/restaurantscene_4.js
@@ -41,14 +41,19 @@ export class Restaurantscene_4 extends Scene {
         // Label toevoegen
         const levelLabel = new Label({
             text: 'Terug naar het begin',
-            pos: new Vector(-89, -10), // 80px naar links binnen de triggerbalk
-            color: Color.White,
-            font: new Font({
-                family: 'Arial',
-                size: 20,
+            pos: new Vector(-89, -5), // 80px naar links binnen de triggerbalk
+            // color: Color.White,
+            // font: new Font({
+            //     family: 'Arial',
+            //     size: 20,
+            //     unit: FontUnit.Px,
+            //     color: Color.White
+            // })
+            font: Resources.PressStart2P.toFont({
+                size: 9,
+                color: Color.fromHex('#00008B'),
                 unit: FontUnit.Px,
-                color: Color.White
-            })
+            }),
         })
         this.#triggerBar.addChild(levelLabel)
         this.add(this.#triggerBar)
@@ -60,13 +65,18 @@ export class Restaurantscene_4 extends Scene {
         // Hoofdtekst: HET EINDE
         const eindLabel = new Label({
             text: 'HET EINDE',
-            pos: new Vector(engine.halfDrawWidth - 250, engine.halfDrawHeight - 40), // 250px naar links
-            font: new Font({
-                family: 'Arial',
-                size: 64,
-                unit: FontUnit.Px,
+            pos: new Vector(engine.halfDrawWidth - 200, engine.halfDrawHeight - 60), // 250px naar links
+            // font: new Font({
+            //     family: 'Arial',
+            //     size: 64,
+            //     unit: FontUnit.Px,
+            //     color: Color.White,
+            //     bold: true
+            // }),
+            font: Resources.PressStart2P.toFont({
+                size: 45,
                 color: Color.White,
-                bold: true
+                unit: FontUnit.Px,
             }),
             color: Color.White,
             anchor: new Vector(0.5, 0.5)
@@ -75,16 +85,22 @@ export class Restaurantscene_4 extends Scene {
 
         // Subtekst: dankje wel voor het spelen, ik hoop dat je iets hebt geleerd
         const dankLabel = new Label({
-            text: 'dankje wel voor het spelen, ik hoop dat je iets hebt geleerd',
+            text: 'Dank je wel voor het spelen, ik hoop dat je iets hebt geleerd',
             pos: new Vector(engine.halfDrawWidth - 250, engine.halfDrawHeight + 30), // 250px naar links
-            font: new Font({
-                family: 'Arial',
-                size: 28,
+            // font: new Font({
+            //     family: 'Arial',
+            //     size: 28,
+            //     unit: FontUnit.Px,
+            //     color: Color.White
+            // }),
+            font: Resources.PressStart2P.toFont({
+                size: 15,
+                color: Color.White,
                 unit: FontUnit.Px,
-                color: Color.White
             }),
             color: Color.White,
-            anchor: new Vector(0.5, 0.5)
+            anchor: new Vector(0.5, 0.5),
+            maxWidth: 560
         })
         this.add(dankLabel)
     }
